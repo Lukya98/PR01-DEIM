f2366f4266f41ba319563302d9ab642fe00c531e
Lucía <luciahoyas1@gmail.com>
2021-09-17 18:34:13 +0200

    ejercicio 2 b

--- UnityClase/Assets/Scripts/Ejercicio2_estructuras.cs
+++ UnityClase/Assets/Scripts/Ejercicio2_estructuras.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class Ejercicio2_estructuras : MonoBehaviour
 {
     private int numero = 0;
-
+    private int numero1 = 1;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        for (numero1 = 1; numero1 < 21; numero1++)
+        {
+            print(numero1);
+        }
         
 
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        while (numero<10)
-        {
-            numero++;
-            print(numero);
-
-        }
+        
     }
 }
