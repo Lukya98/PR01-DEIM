792758dff612b0c787556d4b8288efd6549f87d8
Lucía <luciahoyas1@gmail.com>
2021-09-17 17:58:20 +0200

    Ejercicio 2 a

---
+++ UnityClase/Assets/Scripts/Ejercicio2_estructuras.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_estructuras : MonoBehaviour
+{
+    private int numero = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        while (numero<10)
+        {
+            numero++;
+            print(numero);
+
+        }
+    }
+}
