83f5c9aa18d6decd7f0030c5cbb6126654eff422
lucia <luciahoyas1@gmail.com>
2021-09-24 18:30:25 +0200

    Ejercicio 4b

--- UnityClase/Assets/Scripts/Ejercicio4.cs
+++ UnityClase/Assets/Scripts/Ejercicio4.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 
 public class Ejercicio4 : MonoBehaviour
 {
-    /*a) En el anterior apartado has creado varios métodos, ¿serías capaz de lanzarlos cuando se
- pulse un botón (por ejemplo, que el contador se lance al pulsar un botón y se detenga con
- otro, aunque lo ideal es que sea con el mismo botón y usando una booleana)*/
-
-    [SerializeField] Text Mytext;
+    /*b) Añade un GameObject a la escena y haz que se desplace según los controles de tu
+GamePad, tanto horizontalmente como verticalmente*/
+    [SerializeField] float speed = 5;
+    
 
 
     // Start is called before the first frame update
@@ -21,42 +20,13 @@ public class Ejercicio4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton0))
-        {
-            StartCoroutine("metodo1");
-        }
-        if (Input.GetKeyDown(KeyCode.JoystickButton1))
-        {
-            StartCoroutine("Cronometro");
-        }
-        if (Input.GetKeyDown(KeyCode.JoystickButton2))
-        {
-            StopCoroutine("Cronometro");
-        }
+        float desplY = Input.GetAxis("Vertical") * speed;
+        transform.Translate(Vector3.up * desplY * Time.deltaTime);
+        float desplX = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector3.right * desplX * Time.deltaTime);
 
 
     }
-    //Metodo 1
-    void metodo1()
-    {
-        int n1 = 1;
-        int n2 = 2;
-        int resultado0 = n1 + n2;
-        print(resultado0);
-        return;
-
-    }
-    //Contador
-    IEnumerator Cronometro()
-    {
-        for (int n = 0; n <= 100; n++)
-        {
-            Mytext.text = "Contador: " + n;
-            yield return new WaitForSeconds(1f);
-            if (n == 9)
-            {
-                StopCoroutine("Cronometro");
-            }
-        }
-    }
+    
+    
 }
